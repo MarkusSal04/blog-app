032231d5ef80b460706d422b44b73127c90c975a
Markus Salgado <markus@Markuss-MacBook-Pro.local>
2021-06-22 16:53:19 -0400

    npms intalled, folders and files created and filled

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import logger from 'morgan'
+
+const PORT = process.env.PORT || 3000
+
+import db from './db/connection/js'
+import Post from './models/post.js'
+
+const app = express()
+
+app.use(express.json())
+app.use(logger('dev'))
+
+db.on('connected', () => {
+  console.log('Connected to MongoDB')
+  app.listen(PORT, () =>
+    console.log(`Express server application is running on port ${PORT}`)
+  )
+})
+
+app.get('/', (req, res) => {
+  try {
+    const.posts = await Post.find()
+    res.json(posts)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
+app.get
